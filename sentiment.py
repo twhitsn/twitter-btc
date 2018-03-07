f13ffb8b66ee3b15e38b8f4bb6e9dea78ed47651
Tim Whitson <whitstd@gmail.com>
2018-03-06 23:03:14 -0500

    update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,5 +1,19 @@
 import pandas as pd
+from textblob import TextBlob
 
 tweets = pd.read_csv('data.csv')
 
-print(tweets['text'])
+polarity = []
+
+print('Computing sentiment ...')
+
+for i, tweet in enumerate(tweets['text']):
+    print('{}%'.format(round(i / len(tweets) * 100)), end = '\r')    
+    
+    if type(tweet) is str:
+        blob = TextBlob(tweet)
+        polarity.append(blob.sentiment.polarity)
+    else:
+        polarity.append('')
+    
+print(polarity)
