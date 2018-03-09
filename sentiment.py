3882f24e9c9db7c568bf0ac19a5ca4daca501c6f
deba21 <debashish350@gmail.com>
2018-03-08 19:08:14 -0700

    New sentiment column added to a new o/p file
    A new sentiment column  'Sentiment_Polarity' added to a new o/p file data_with_sentiment.

--- sentiment.py
+++ sentiment.py
@@ -2,6 +2,7 @@ import pandas as pd
 from textblob import TextBlob
 
 tweets = pd.read_csv('data.csv')
+tweets['Sentiment_Polarity'] = 'NA'
 
 polarity = []
 
@@ -13,7 +14,9 @@ for i, tweet in enumerate(tweets['text']):
     if type(tweet) is str:
         blob = TextBlob(tweet)
         polarity.append(blob.sentiment.polarity)
+        tweets.ix[i, 'Sentiment_Polarity'] = blob.sentiment.polarity
     else:
         polarity.append('')
     
+tweets.to_csv('data_with_sentiment.csv')
 print(polarity)
