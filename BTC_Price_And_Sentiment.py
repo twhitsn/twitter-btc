a6c362d77ba590042151ad012968bef5e8110612
deba21 <debashish350@gmail.com>
2018-03-08 20:45:03 -0700

    Create BTC_Price_And_Sentiment.py
    Initial commit

---
+++ BTC_Price_And_Sentiment.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+tweets = pd.read_csv('data_with_sentiment.csv')
+tweets['date'], tweets['time'] = tweets['timestamp'].str.split('T', 1).str
+
+BTC_price = pd.read_csv('bitcoin_market_price_2017.csv')
+groups = tweets.groupby(['date']).mean().reset_index()
+
+data_merged = pd.merge(BTC_price, groups, on="date")
+
+columns = ['id', 'likes', 'replies', 'retweets']
+data_merged.drop(columns, inplace=True, axis=1)
+data_merged.rename(columns={'Sentiment_Polarity': 'Mean_Sentiment_Polarity'}, inplace=True)
+
+data_merged.to_csv('bitcoin_market_price_2017.csv', index=False)
