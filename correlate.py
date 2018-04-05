61bcc0347976b696edc73273a2418ee6569925e8
Tim Whitson <whitstd@gmail.com>
2018-04-04 23:25:52 -0400

    add correlate script

---
+++ correlate.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+btc = pd.read_csv('data/bitcoin_market_price_2017.csv')
+btc['date'] = pd.to_datetime(btc['date']).dt.date 
+
+sentiment = pd.DataFrame()
+
+print('Merging sentiment datasets ...')
+
+# merge sentiment data
+for i in range(1, 4):
+    cur_sentiment = pd.read_csv('data/sentiment{}.csv'.format(i))
+    cur_sentiment['date'] = pd.to_datetime(cur_sentiment['timestamp']).dt.date 
+    sentiment = sentiment.append(cur_sentiment)    
+    
+print('Grouping sentiment dataset by day ...')
+
+# get daily mean polarity
+daily = sentiment.groupby('date', as_index = False)['polarity'].mean()
+daily = daily.merge(btc, how = 'inner', on = 'date')
+
+# add lags
+lags = 2
+
+for i in range(1, lags + 1): 
+    daily['close-{}'.format(i)] = daily['close'].shift(i).values
+
+print('Computing correlation matrix ...') 
+print(daily.corr())
+
+
